d35dfb841e049fced541574edfc654af69638713
XanderReynolds <xane514@gmail.com>
2021-05-08 15:09:44 -0400

    search is working now

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ const appendToCardList = (htmlString) => {
 
 const createThermoCard = (thermoCard) => {
   // <!-- Thermometer Card -->
-  const newThermoDevice = `<div class="device-card">
+  const newThermoDevice = `<div data-item="${thermoCard.deviceLocation}" class="device-card">
     <div class="device-card-header">
       <h2>${thermoCard.deviceLocation}</h2>
       <div class="thermometer-icon-wrapper">
@@ -22,7 +22,7 @@ const createThermoCard = (thermoCard) => {
 
 const createVentCard = (ventCard) => {
 //<!-- Vent Card -->
-  const newVent = `<div class="device-card">
+  const newVent = `<div data-item="${ventCard.controlName}" class="device-card">
     <div class="device-card-header">
       <h2>${ventCard.controlName}</h2>
       <div class="vent-icon-wrapper">
@@ -35,11 +35,26 @@ const createVentCard = (ventCard) => {
   appendToCardList(newVent);
 }
 
-
-
 const servoDevices = getRemoteServoDevices();
 const tempDevices = getRemoteTemperatureDevices();
+
 
 tempDevices.forEach((thermoDevice) => createThermoCard(thermoDevice));
 servoDevices.forEach((servoDevice) => createVentCard(servoDevice));
 
+const searchBox = document.getElementById("search");
+
+ searchBox.addEventListener('keyup', (e) => {
+  const searchInput = e.target.value.toLowerCase().trim();
+  
+  const cards = document.getElementsByClassName("device-card"); 
+  Array.from(cards).forEach( (card) => {    
+    const cardString = card.dataset.item.toLowerCase();
+
+    if(cardString.includes(searchInput.toLowerCase())){
+      card.style.display = 'block';
+    } else {
+      card.style.display = 'none';
+    }
+  })  
+})
