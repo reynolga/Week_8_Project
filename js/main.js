746290cd90d761c657af8425069f67a36344d5bc
XanderReynolds <xane514@gmail.com>
2021-05-08 14:24:01 -0400

    Cards are dynamically created now

--- js/main.js
+++ js/main.js
@@ -1,4 +1,45 @@
 import {getRemoteServoDevices, getRemoteTemperatureDevices} from './jsonFile.js';
 
+const appendToCardList = (htmlString) => {
+  const cardList = document.getElementById('device-card-flexbox');  
+  cardList.innerHTML += htmlString; 
+}
+
+const createThermoCard = (thermoCard) => {
+  // <!-- Thermometer Card -->
+  const newThermoDevice = `<div class="device-card">
+    <div class="device-card-header">
+      <h2>${thermoCard.deviceLocation}</h2>
+      <div class="thermometer-icon-wrapper">
+        <i class="fas fa-thermometer-half"></i>
+      </div>
+    </div>
+    <p>${thermoCard.currentTemp.toFixed(1)} &#8457</p>
+  </div>`
+  
+  appendToCardList(newThermoDevice);
+}
+
+const createVentCard = (ventCard) => {
+//<!-- Vent Card -->
+  const newVent = `<div class="device-card">
+    <div class="device-card-header">
+      <h2>${ventCard.controlName}</h2>
+      <div class="vent-icon-wrapper">
+        <i class="fab fa-elementor"></i>
+      </div>
+    </div>
+    <p>${ventCard.currentSetPosition}%</p>      
+  </div>`;
+
+  appendToCardList(newVent);
+}
+
+
+
 const servoDevices = getRemoteServoDevices();
 const tempDevices = getRemoteTemperatureDevices();
+
+tempDevices.forEach((thermoDevice) => createThermoCard(thermoDevice));
+servoDevices.forEach((servoDevice) => createVentCard(servoDevice));
+
