5358feb6cb64f03a2fedfbd13a81c4c5fdbcb7f0
XanderReynolds <xane514@gmail.com>
2021-05-08 16:36:58 -0400

    Added more sorting options

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ const appendToCardList = (htmlString) => {
 
 const createThermoCard = (thermoCard) => {
   // <!-- Thermometer Card -->
-  const newThermoDevice = `<div data-item="${thermoCard.deviceLocation}" data-deviceType="thermometer" class="device-card">
+  const newThermoDevice = `<div data-item="${thermoCard.deviceLocation}" data-deviceType="temperature" class="device-card">
     <div class="device-card-header">
       <div class="thermometer-icon-wrapper">
         <i class="fas fa-thermometer-half"></i>
@@ -37,6 +37,7 @@ const createVentCard = (ventCard) => {
 
 const servoDevices = getRemoteServoDevices();
 const tempDevices = getRemoteTemperatureDevices();
+const dataFilter = '[data-filter]';
 
 tempDevices.forEach((thermoDevice) => createThermoCard(thermoDevice));
 servoDevices.forEach((servoDevice) => createVentCard(servoDevice));
@@ -56,4 +57,128 @@ searchBox.addEventListener('keyup', (e) => {
       card.style.display = 'none';
     }
   })  
-})
+});
+
+
+const filterLink = document.querySelectorAll(dataFilter);
+const active = 'active';
+
+const setActive = (elm, selector) => {
+  if(document.querySelector(`${selector}.${active}`) !== null){
+    document.querySelector(`${selector}.${active}`).classList.remove(active);
+  }
+  elm.classList.add(active);  
+};
+
+const showAllCards = () => {
+  const cards = document.getElementsByClassName("device-card"); 
+  Array.from(cards).forEach( (card) => { 
+    card.style.display = 'block';
+  })
+}
+
+const hideAllCards = () => {
+  const cards = document.getElementsByClassName("device-card"); 
+  Array.from(cards).forEach( (card) => { 
+    card.style.display = 'none';
+  })
+}
+
+
+
+const showCardsOfType = (type) => {
+  const cards = document.getElementsByClassName("device-card"); 
+  Array.from(cards).forEach((card) => { 
+    if(card.dataset.devicetype == type){
+      card.style.display = 'block';
+    } else{
+      card.style.display = 'none';
+    }
+  })
+}
+
+const sortCardsByTemperature = () => {
+  const sortedList = tempDevices.sort(function(a,b) {
+    return a.currentTemp - b.currentTemp;
+  })
+  //delete cards
+  const cardList = document.getElementById('device-card-flexbox');  
+  cardList.innerHTML = ""; 
+
+  //create cards
+  sortedList.forEach((thermoDevice) => createThermoCard(thermoDevice));
+  servoDevices.forEach((servoDevice) => createVentCard(servoDevice));
+}
+
+const sortCardsByOpenPosition = () => {
+  const sortedList = servoDevices.sort(function(a,b) {
+    return a.currentSetPosition - b.currentSetPosition;
+  })
+  //delete cards
+  const cardList = document.getElementById('device-card-flexbox');  
+  cardList.innerHTML = ""; 
+
+  //create cards
+  sortedList.forEach((servoDevice) => createVentCard(servoDevice));
+  tempDevices.forEach((thermoDevice) => createThermoCard(thermoDevice)); 
+}
+
+const sortByRoom = () => {
+  //Delete cards
+  const cardList = document.getElementById('device-card-flexbox');  
+  cardList.innerHTML = ""; 
+
+  const servoNames = servoDevices.map((device) => { return {name:device.controlName, type:'servo'} }); 
+  const tempNames = tempDevices.map((device) => { return {name:device.deviceLocation, type:'temp'} }); 
+
+  const allNames = [...servoNames, ...tempNames];
+  allNames.sort(function(a,b) {
+    if(a.name < b.name) return -1;
+    if(a.name > b.name) return 1;
+    return 0;
+  });
+
+  allNames.forEach((listItem) => {
+    
+    //find which device
+    if(listItem.type == 'servo') {
+        //Find it.
+        const servoDevice = servoDevices.find(servo => servo.controlName === listItem.name);        
+        //build it.
+        if(servoDevice != undefined) {
+          createVentCard(servoDevice);
+        }
+    }else if(listItem.type == 'temp'){
+        //Find it.
+        const tempDev = tempDevices.find(servo => servo.deviceLocation === listItem.name);        
+        //build it.
+        if(tempDev != undefined) {
+          createThermoCard(tempDev);
+        }
+    }
+  })
+}
+
+for(const filter of Array.from(filterLink))
+{
+  filter.addEventListener("click", function(filter){
+    //Set active
+    setActive(this, '.filter-label');
+
+    //filter values
+    if(this.dataset.filter == 'all'){
+      showAllCards();
+    }else if(this.dataset.filter == 'sortByTemp'){
+      sortCardsByTemperature();
+    }else if(this.dataset.filter == 'sortByOpen'){
+      sortCardsByOpenPosition();
+    }else if(this.dataset.filter == 'sortByRoom'){
+      sortByRoom();
+    }else if (this.dataset.filter == 'vent'){
+      showCardsOfType('vent');
+    }else if (this.dataset.filter == 'temperature'){
+      showCardsOfType('temperature');
+    }
+  })
+}
+
